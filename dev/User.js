3160d6b3ea79689ecd1843540ce35df157bbbb51
Sebastien Oillic <sebastien.oillic@orange.fr>
2017-04-04 21:53:18 +0200

    console.log not working. Cannot debug.

--- dev/User.js
+++ dev/User.js
@@ -3,80 +3,95 @@ import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 export default class UserGestion{
+    constructor(){
+        var userSchema = new Schema({
+            username: String,
+            password: String
+        }, {collection: 'user'});
+        this.User = mongoose.model('user', userSchema);
+    }
     connect(callback){
         // mongoose.createConnection('mongodb://162.243.195.173:27017/group2');
-        let db = mongoose.createConnection('mongodb://localhost:27017/account');
+        var db = mongoose.createConnection('mongodb://localhost:27017/account');
         // let db = mongoose.connection;
         db.on('error', console.error.bind(console, 'connection error:'));
         db.once('open', () =>{
             console.log('Connected To Mongo Database');
-            let userSchema = new Schema({
-                username: String,
-                password: String
-            });
-            mongoose.model(('User', userSchema));
-            let User = mongoose.model('User');
-            callback(db, User);
+            callback(db)
         });
         
     }
     addUser(username, password){
         // mongoose.connect('mongodb://162.243.195.173:17027/group2');
-        this.connect((db, User)=>{
-            User.find({name: username}, (err, user) => {
+        this.connect((db)=>{
+            console.log("add user");
+            let query = this.User.find({username: username})
+            query.exec((err, user) => {
                 if (err){
-                    mongoose.disconnect();
+                     
                     console.log(err);
                     return 0;
                 }
                 if (user != []){
-                    mongoose.disconnect();
+                     
                     console.log('User already exists!')
                     return 0;
                 }
                 else{
                     console.log("User creation...");
-                    let newUser = new User({username: username, password: password});
+                    let newUser = new this.User({username: username, password: password});
                     newUser.save((err, newUser)=>{
                         if (err){
-                            mongoose.disconnect();
+                             
                             console.log(err);
                             return 0;
+                        }
+                        else{
+                             
+                            console.log("User created!");
+                            return 1;
                         }
                     })
                 }
             });
         });
-        return 1;
-
+        mongoose.disconnect();
     }
     checkPass(username, password){
-        this.connect((db, User)=>{
-            User.find({name: username}, (err, user) => {
+        this.connect((db)=>{
+            console.log("checkPass");
+            // console.log(this);
+            // console.log(this.User.find());
+            let query = this.User.find({username: username});
+            // console.log(query);
+            query.exec((err, user) => {
+                // console.log("test");
                 if (err){
-                    mongoose.disconnect();
+                     
                     console.log(err);
                     return 0;
                 }
                 if (user.length != 1){
-                    mongoose.disconnect();
+                     
                     console.log('User does not exists!');
                     return 0;
                 }
                 else{
                     if (user[0].password != password){
-                        mongoose.disconnect();
+                         
                         console.log("Bad credentials!");
                         return 0;
                     }
                     else{
                         console.log("Correct credentials!");
-                        mongoose.disconnect();
+                         
                         return 1;
                     }
                 }
             });
-            
+            // console.log(result);
+            // console.log("loooool Nope!");
         });
+        mongoose.disconnect();
     }
 }
