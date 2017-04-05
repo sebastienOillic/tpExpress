3f78014cf6ca8bfca9622b155b090786b7b6ccfc
Richard Coudrais <richard.coudrais@gmail.com>
2017-04-05 09:36:32 +0200

    finalisation generator

--- dev/index.js
+++ dev/index.js
@@ -2,30 +2,30 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 
-import IndexCtrl from './controllers/IndexCtrl';
-import GeneratorCtrl from './controllers/GeneratorCtrl';
 import LoginCtrl from './controllers/LoginCtrl';
+import RegisterCtrl from './controllers/RegisterCtrl';
+import GeneratorCtrl from './controllers/GeneratorCtrl';
 
-const indexCtrl = new IndexCtrl();
+const registerCtrl = new RegisterCtrl();
 const generatorCtrl = new GeneratorCtrl();
 const loginCtrl = new LoginCtrl();
 
 const app = express();
-
 const port = process.argv[2] || 8080;
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '../views/pages'));
 
-app.get('/', indexCtrl.indexAction);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/generator', generatorCtrl.indexAction);
-app.post('/generator', generatorCtrl.indexActionPost);
+app.get('/', registerCtrl.registerAction);
+app.post('/', registerCtrl.registerActionPost);
+app.get('/generator', generatorCtrl.generatorAction);
+app.post('/generator', generatorCtrl.generatorActionPost);
 app.get('/login', loginCtrl.loginAction);
 app.post('/login', loginCtrl.loginActionPost);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
     console.log('Server is listening on port ' + port)
